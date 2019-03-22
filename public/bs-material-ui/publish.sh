0f71f793af2440ccaf21d3555c928983cc1c9cb2
Jonathan Siebern <jsiebern88@gmail.com>
2019-03-22 14:20:40 +0100

    [FIX] Fixes regression where readme is not visible on npm

--- public/bs-material-ui/publish.sh
+++ public/bs-material-ui/publish.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 mv ./bsconfig.json ./bsc.json; mv ./bsconfig-npm.json ./bsconfig.json
+cp ./../../README.md ./README.md
 yarn publish
 mv ./bsconfig.json ./bsconfig-npm.json; mv ./bsc.json ./bsconfig.json
+rm ./README.md
