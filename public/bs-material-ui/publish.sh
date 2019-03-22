5883b4e32355cea4a9d26dfba9007c2826d4c01e
Jonathan Siebern <jsiebern88@gmail.com>
2019-03-22 14:03:55 +0100

    -

--- public/bs-material-ui/publish.sh
+++ public/bs-material-ui/publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mv ./bsconfig.json ./bsc.json; mv ./bsconfig-npm.json ./bsconfig.json
-yarn publish
+yarn publish --tag hooks
 mv ./bsconfig.json ./bsconfig-npm.json; mv ./bsc.json ./bsconfig.json
