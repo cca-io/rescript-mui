9b97071f684708b70b9bff7fcb758c4319db58c3
Jonathan Siebern <jsiebern88@gmail.com>
2020-08-16 05:10:02 +0200

    Clean up package versions and dependencies

--- public/bs-material-ui/publish.sh
+++ public/bs-material-ui/publish.sh
@@ -2,6 +2,6 @@
 rm src/*.bs.js
 mv ./bsconfig.json ./bsc.json; mv ./bsconfig-npm.json ./bsconfig.json
 cp ./../../README.md ./README.md
-yarn publish --access public --tag zero
+yarn publish --access public
 mv ./bsconfig.json ./bsconfig-npm.json; mv ./bsc.json ./bsconfig.json
 rm ./README.md
