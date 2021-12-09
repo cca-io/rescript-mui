02da10cb486ab0aa68e2abfb415ace1b7eb83edb
Florian Hammerschmidt <fh@cca.io>
2021-12-09 14:54:01 +0100

    Binding-Generator: fix int enum handling

--- examples/src/examples/ExampleSlider.bs.js
+++ examples/src/examples/ExampleSlider.bs.js
@@ -35,33 +35,7 @@ function ExampleSlider(Props) {
                   alignItems: "center",
                   children: null,
                   container: true,
-                  spacing: (function () {
-                        switch (2) {
-                          case "0" :
-                              return 0;
-                          case "1" :
-                              return 1;
-                          case "2" :
-                              return 2;
-                          case "3" :
-                              return 3;
-                          case "4" :
-                              return 4;
-                          case "5" :
-                              return 5;
-                          case "6" :
-                              return 6;
-                          case "7" :
-                              return 7;
-                          case "8" :
-                              return 8;
-                          case "9" :
-                              return 9;
-                          case "10" :
-                              return 10;
-                          
-                        }
-                      })()
+                  spacing: 2
                 }, React.createElement(Core.Grid, {
                       children: React.createElement(VolumeUp, {}),
                       item: true
