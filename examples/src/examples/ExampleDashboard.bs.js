d5e75dcb02106e4bc4b372da696d1d7ee70b7b81
Florian Hammerschmidt <florianh89@gmail.com>
2025-12-02 18:22:40 +0100

    Some upgrades

--- examples/src/examples/ExampleDashboard.bs.js
+++ examples/src/examples/ExampleDashboard.bs.js
@@ -68,13 +68,13 @@ function ExampleDashboard(props) {
                                                 children: "Dashboard"
                                               }),
                                           JsxRuntime.jsx(Link, {
-                                                color: "text.secondary",
-                                                children: "Close Dashboard",
-                                                href: "#",
                                                 onClick: (function (e) {
                                                     e.preventDefault();
                                                     setShow(false);
-                                                  })
+                                                  }),
+                                                color: "text.secondary",
+                                                children: "Close Dashboard",
+                                                href: "#"
                                               }),
                                           JsxRuntime.jsx(IconButton, {
                                                 children: Caml_option.some(JsxRuntime.jsx(Badge, {
